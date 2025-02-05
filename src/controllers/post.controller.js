b302714990cd86ff1dd35371ccd3cc1e951b5c1d
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-02-05 20:57:58 +0900

    fix :: 에러 반환 추가

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,6 +1,15 @@
 const { postService } = require("./services/post.service");
 
 const postController = async (req, res) => {
-    await postService(); // await은 뒤에오는 함수의 Promise(일종의 하나의 class)가 반환될때 까지 기다림 -> 동기적으로 하기
-    res.end(); // return 같은 거
-}
+    try {
+
+        await postService(); // await은 뒤에오는 함수의 Promise(일종의 하나의 class)가 반환될때 까지 기다림 -> 동기적으로 하기
+        res.statusCode = 200;
+        res.end(); // return 같은 거
+    } catch (error) {
+        res.statusCode = 500;
+        res.end(JSON.stringify({ message: error.message }));
+    }
+};
+
+module.exports = { postController };
