bc43ac9afab73f10c3db225a9aa17c3fe559f07f
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-02-06 11:25:17 +0900

    fix :: mysql.js

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,12 +1,22 @@
-var mysql = require('mysql');
+const mysql = require('mysql2');
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'smc0416!@',
-  database : 'node-post'
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'smc0416!@',
+    database: 'node-post'
 });
 
 connection.connect();
+
+connection.query('SELECT * FROM tb_post', function (error, results, fields) {
+    if (error) {
+        console.log("쿼리 실행 중 오류 발생:", error);
+        return;
+    }
+    console.log("조회된 데이터:", results);
+});
+   
+connection.end();
 
 module.exports = connection;
