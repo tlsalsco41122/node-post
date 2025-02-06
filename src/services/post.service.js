b6b792808528aed932938ad2631588ece2c44e13
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-02-06 09:41:47 +0900

    fix :: post.service 예외처리 추가

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,18 +1,23 @@
 const { mysql } = require("./db/mysql");
 
 const createDataService = async (data) => {
-    const { title, content } = data;
-    console.log(title, content);
-
-    const{ data: createData, error: createError }  = await mysql
-        .from("tb_post") // "tb_post" 테이블에 데이터를 삽입하겠다는 의미
-        .insert({ // 아마도 값을 넣는 것?
-            title,
-            content
-        });
-
-    if (createError) {
-        console.log("데이터 추가중 오류:", createError.message);
+    try{ // try-catch로 예외 처리하기
+        const { title, content } = data;
+        console.log(title, content);
+    
+        const{ data: createData, error: createError }  = await mysql
+            .from("tb_post") // "tb_post" 테이블에 데이터를 삽입하겠다는 의미
+            .insert({ // 아마도 값을 넣는 것?
+                title,
+                content
+            });
+    
+        if (createError) {
+            throw new Error(createError.message); // 에러를 던짐
+        };
+        console.log("goooooood");
+    } catch (error){
+        console.log("데이터 삽입 중 오류", error.message);
     };
 };
 
