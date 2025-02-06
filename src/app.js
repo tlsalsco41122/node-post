81755f5be9d1a61bcd634faad215f72f9d6a575f
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-02-06 10:02:47 +0900

    feat :: app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const { postController } = require("./controllers/post.controller"); // 컨트롤러
+const { createData } = require("./controllers/post.controller");
 
 // async가 있어야 await이 사용 가능
 const server = http.createServer(async(req, res) => {
@@ -9,8 +9,30 @@ const server = http.createServer(async(req, res) => {
 
     try{
         if (req.method === "POST") {
-            if.url 
-        }
-    }
+            if (req.url === "/create") {
+                let body = "";
+                req.on("data", (chunk) => { // 클라이언트가 보낸 데이터를 조금씩(chunk 단위로) 받을 때마다 실행됨
+                    body += chunk; // body 변수에 데이터가 쌓임.
+                });
 
+                req.on("end", async () => { // 모든 데이터 수신이 끝나면 실행됨
+                    try {
+                        const parsedBody = JSON.parse(body); // json 형식으로 변환
+                        await createData(req, res, parsedBody);
+                    } catch (parseError) {
+                        res.end(JSON.stringify({ message: "JSON 변경중 오류 발생" }));
+                    }
+                });
+                return;
+            }
+        }
+    } catch (err)  {
+        if (!res.headersSent) {
+            res.end(
+                JSON.stringify({ message: `Internal Server Error, ${err.message}` })
+            );
+        };
+    }
 });
+
+module.exports = server;
