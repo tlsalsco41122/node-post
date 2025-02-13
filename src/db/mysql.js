d5977d9ad717d5a72e012ab508f11d80d539bbd5
신민채 <tlsalsco41122@gmail.com>
2025-02-13 21:09:44 +0900

    Update mysql.js

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 //mysql.createConnection() 방식과 mysql.createPool() 방식이 다음 -> 계속 에러;;;
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: 'smc0416!@',
-    database: 'node-post',
+    user: 'user',
+    password: 'password',
+    database: 'database',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
